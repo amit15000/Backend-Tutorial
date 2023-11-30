5f66ad5add89b200ea476f3abca78b4a395b5a5d
Amit Kumar <15000amitkumar@gmail.com>
2023-11-30 19:02:04 +0530

    Created a basic server with routes

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+
 const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 app.listen(3000, () => {
   console.log("Server of Amit started at 3000");
 });
 
 app.get("/", (request, response) => {
   response.send("Hello jee");
+});
+app.post("/api/cars", (req, res) => {
+  const { name, brand } = req.body;
+  res.send("Car's post request completed.");
 });
