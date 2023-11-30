c3e5ebdebe49c5c38caddade6637f583e27f40d9
Amit Kumar <15000amitkumar@gmail.com>
2023-11-30 10:54:55 +0530

    modfied import

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-import express from "express";
+const express = require("express");
 const app = express();
 app.listen(3000, () => {
   console.log("Server of Amit started at 3000");
+});
+
+app.get("/", (request, response) => {
+  response.send("Hello jee");
 });
