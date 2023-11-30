858a16cb10bdf820eb863fde9bd72d6693ad2f03
Amit Kumar <15000amitkumar@gmail.com>
2023-11-30 21:29:13 +0530

    Server setup and connected to database

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
 const bodyParser = require("body-parser");
@@ -16,3 +16,11 @@ app.post("/api/cars", (req, res) => {
   const { name, brand } = req.body;
   res.send("Car's post request completed.");
 });
+mongoose
+  .connect("mongodb://localhost:27017/myDatabase")
+  .then(() => {
+    console.log("Server Connection with Database : Successful");
+  })
+  .catch((error) => {
+    console.log("Server Connection With Database :Failed");
+  });
